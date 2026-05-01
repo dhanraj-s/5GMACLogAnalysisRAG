d42cf114ab34e80818bb0f650457027835fd2b95
dhanraj-s <dhanrajshrivastav28@gmail.com>
2026-05-01 21:15:48 +0530

    parsing and extracting important events from logs

---
+++ twin_rag/mine_logs.py
@@ -0,0 +1,77 @@
+from drain3 import TemplateMiner
+from drain3.template_miner_config import TemplateMinerConfig
+import re
+
+def clean_log_line(line):
+    ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
+    line = ansi_escape.sub('', line)
+
+    line = re.sub(r'\[NR_\w+\]', '', line)
+    line = re.sub(r'\[MAC\]', '', line)
+    line = re.sub(r'CQI\s+0\b', 'CQI <CQI_INVALID>', line, flags=re.IGNORECASE)
+    line = re.sub(r'MCS\s+\(\d+\)\s+0\b', 'MCS <MCS_INVALID>', line, flags=re.IGNORECASE)
+    line = re.sub(r'MCS\s+0\b', 'MCS <MCS_INVALID>', line, flags=re.IGNORECASE)
+
+    line = ' '.join(line.split())
+
+    return line.strip()
+
+def score_templates(template_freq, templates, log_lines):
+    total_lines = len(log_lines)
+
+    SIGNIFICANT = [
+        "fail", "invalid", "loss", "drop",
+        "timeout", "exceed", "mismatch", "unknown",
+        "abort", "reject", "release", "disconnect",
+        "retx", "nack", "corrupt"
+    ]
+
+    scores = {}
+
+    for template, count in template_freq.items():
+
+        rarity_score = 1 - (count / total_lines)
+
+        significance_score = sum(
+            1 for word in SIGNIFICANT
+            if word in template.lower()
+        )
+        
+        scores[template] = rarity_score * (significance_score + 1)
+
+    return sorted(scores.items(), key=lambda x: x[1], reverse=True)
+
+def get_scored_events(log_filename):
+    config = TemplateMinerConfig()
+    config.parametrize_numeric_tokens = True
+    miner = TemplateMiner(config=config)
+    templates = []
+
+    with open(log_filename, 'r') as f:
+        log_lines = [clean_log_line(line) for line in f.read().split('\n') if line != '']
+    
+    for line in log_lines:
+        result = miner.add_log_message(line)
+
+    templates = []
+    for line in log_lines:
+        result = miner.match(line)
+        if result:
+            templates.append(result.get_template())
+        else:
+            templates.append("UNMATCHED")
+
+    template_freq = dict()
+    for template in templates:
+        template_freq[template] = template_freq.get(template,0) + 1
+    
+    return score_templates(template_freq, templates, log_lines)
+
+
+if __name__ == '__main__':
+    scored_events = get_scored_events('log_files/du_mac_logs_3.txt')
+    for ev in scored_events:
+        print(ev)
+    
+
+
