2b4fe26bfee77394eb91abecc22a976023736e5a
dhanraj-s <43548871+dhanraj-s@users.noreply.github.com>
2026-05-01 20:38:31 +0530

    Update convert_to_markdown.py
    Add heading level fixes for 3gpp docs

--- twin_rag/convert_to_markdown.py
+++ twin_rag/convert_to_markdown.py
@@ -1,4 +1,6 @@
-from docling.document_converter import DocumentConverter
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions, AcceleratorOptions
 import os, re, html
 
 INPUT_DIR = 'docs/'
@@ -17,6 +19,28 @@ SRC_FILES = [f for f in os.listdir(INPUT_DIR) if re.search(r'\.md$', f) is None]
 converter = DocumentConverter()
 results = [converter.convert(INPUT_DIR+src) for src in SRC_FILES]
 
+def fix_3gpp_heading(text):
+    lines = text.split('\n')
+
+    fixed_lines = []
+    for line in lines:
+        if line.startswith('##'):
+            line = line.strip()
+            heading = ' '.join(line.split(' ')[1:])
+            
+            if re.match(r'\d+\.\d+\.\d+\.\d+\.\d+\.\d+', heading):
+                line = '###### ' + heading
+            elif re.match(r'\d+\.\d+\.\d+\.\d+\.\d+', heading):
+                line = '##### ' + heading
+            elif re.match(r'\d+\.\d+\.\d+\.\d+', heading):
+                line = '#### ' + heading
+            elif re.match(r'\d+\.\d+\.\d+', heading):
+                line = '### ' + heading
+            
+        fixed_lines.append(line)
+    return '\n'.join(fixed_lines)
+
+
 for i,result in enumerate(results):
     with open(OUTPUT_DIR+SRC_FILES[i]+'.md', 'w') as f:
-        f.write(html.unescape(result.document.export_to_markdown(strict_text=True)))
+        f.write(fix_3gpp_heading(html.unescape(result.document.export_to_markdown(strict_text=True))))
